e60d62c68ab44a5c058c131aaff0d8227aa794da
Carlory <ecarlory@gmail.com>
2025-11-06 12:54:37 -0500

    Classe Weapon_Mod

--- src/main/java/org/example/Weapon.java
+++ src/main/java/org/example/Weapon.java
@@ -3,26 +3,26 @@ package org.example;
 // I'm putting the class in the final position to avoid inheritance
 // Since the object is immutable (all fields are final and there are no setters)
 public final class Weapon {
-    private final String nom;
-    private final int puissance;
+    private final String name;
+    private final int power;
 
     //constructor and validation, an error is raised immediately if the data is invalid.
-    public Weapon(String nom, int puissance) {
-        if (puissance <= 0) throw new IllegalArgumentException("La puissance doit etre positive");
-        this.nom = nom;
-        this.puissance = puissance;
+    public Weapon(String name, int power) {
+        if (power <= 0) throw new IllegalArgumentException("La puissance doit etre positive");
+        this.name = name;
+        this.power = power;
     }
 
     public String getNom() {
-        return nom;
+        return name;
     }
 
     public int getPuissance() {
-        return puissance;
+        return power;
     }
 
     @Override
     public String toString(){
-        return nom + "(Puissance "+ puissance + ")";
+        return name + "(Puissance "+ power + ")";
     }
 }
