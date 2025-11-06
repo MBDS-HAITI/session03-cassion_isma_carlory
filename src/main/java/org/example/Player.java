25c34592e434d18dcab5aa1b31d342783961cfcb
thecassion <pierrerobentz.cassion@gmail.com>
2025-11-06 18:56:48 -0500

    Adding methodes to add one or multiple characters

--- src/main/java/org/example/Player.java
+++ src/main/java/org/example/Player.java
@@ -1,18 +1,18 @@
 package org.example;
 
-import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
 public  class Player {
-    private final List<Character> characters;
-    private static Set<String> names;
+    private List<Character> characters;
+    private static Set<String> names = new HashSet<>();
     private final String name;
+    private Set<String> characterTypes = new HashSet<>();
     public Player(String name,List<Character> characters) {
         this.characters = characters;
 //        Verify that the name did not exist in the names property
-        if(names.contains(name)) {
+        if(Player.names.contains(name.toLowerCase())) {
             throw  new IllegalArgumentException("Player already exists");
         }
         if (characters.size() != 3) {
@@ -22,20 +22,50 @@ public  class Player {
             throw new IllegalArgumentException("The type of characters must be unique");
         }
         this.name = name;
-        names.add(name);
+        Player.names.add(name.toLowerCase());
 
+    }
+//    Allow player to be created without Characters
+    public Player(String name) {
+        if(Player.names.contains(name.toLowerCase())) {
+            throw  new IllegalArgumentException("Player already exists");
+        }
+        this.name = name;
+        Player.names.add(name.toLowerCase());
     }
 //  Verify that Character type is unique for the player
     private boolean isCharactersTypeUnique() {
-        Set <String> types = new HashSet<>();
         for (Character character : this.characters) {
-            types.add(character.getType().toLowerCase());
+            this.characterTypes.add(character.getType().toLowerCase());
         }
-        return types.size() == this.characters.size();
+        return this.characterTypes.size() == this.characters.size();
     }
 
     public String getName() {
         return name;
     }
-//    TODO : Implements remaining methods for player
+//    Allow to add a Character
+    public void addCharacter(Character character) {
+        if(!this.characterTypes.contains(character.getType().toLowerCase())) {
+            throw new IllegalArgumentException("The type of characters must be unique");
+        }
+        if(this.characters.size()>=3){
+            throw  new IllegalArgumentException("The number of characters must be 3");
+        }
+        characters.add(character);
+        characterTypes.add(character.getType().toLowerCase());
+
+    }
+//    Allow player to add multiple characters
+    public void addCharacters(List<Character> characters) {
+        for(Character character : characters) {
+            this.addCharacter(character);
+        }
+    }
+//    Allow player to create or instanciate his own character
+//    TODO : add Character using type, character name
+//    I think the type should be an enum
+//    public Character createCharacter() {
+//
+//    }
 }
