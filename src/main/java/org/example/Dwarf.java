639778ea9416b2e87deb7e1b7ae0724371ee5372
thecassion <pierrerobentz.cassion@gmail.com>
2025-11-07 22:03:23 -0500

    Fix types and make constructor more simple to use as said in the Readme 2

--- src/main/java/org/example/Dwarf.java
+++ src/main/java/org/example/Dwarf.java
@@ -2,12 +2,9 @@ package org.example;
 
 public class Dwarf extends Character implements Attacker {
 
-   public Dwarf(String name){
-       super();
-   }
 
-    public Dwarf(String name, Weapon weapon, int hp, String type, boolean isAlive,int maxHp) {
-        super(name, weapon, hp, type,isAlive,maxHp);
+    public Dwarf(String name) {
+        super(name, Type.DWARF);
     }
 
     @Override
--- src/main/java/org/example/Player.java
+++ src/main/java/org/example/Player.java
@@ -6,9 +6,9 @@ import java.util.Set;
 
 public  class Player {
     private List<Character> characters;
-    private static Set<String> names = new HashSet<>();
+    private static final Set<String> names = new HashSet<>();
     private final String name;
-    private Set<String> characterTypes = new HashSet<>();
+    private final Set<Type> characterTypes = new HashSet<>();
     public Player(String name,List<Character> characters) {
         this.characters = characters;
 //        Verify that the name did not exist in the names property
@@ -36,7 +36,7 @@ public  class Player {
 //  Verify that Character type is unique for the player
     private boolean isCharactersTypeUnique() {
         for (Character character : this.characters) {
-            this.characterTypes.add(character.getType().toLowerCase());
+            this.characterTypes.add(character.getType());
         }
         return this.characterTypes.size() == this.characters.size();
     }
@@ -46,14 +46,14 @@ public  class Player {
     }
 //    Allow to add a Character
     public void addCharacter(Character character) {
-        if(!this.characterTypes.contains(character.getType().toLowerCase())) {
+        if(!this.characterTypes.contains(character.getType())) {
             throw new IllegalArgumentException("The type of characters must be unique");
         }
         if(this.characters.size()>=3){
             throw  new IllegalArgumentException("The number of characters must be 3");
         }
         characters.add(character);
-        characterTypes.add(character.getType().toLowerCase());
+        characterTypes.add(character.getType());
 
     }
 //    Allow player to add multiple characters
