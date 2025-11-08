409a2ae854091c71db4dc67973694d90e65a85fd
thecassion <pierrerobentz.cassion@gmail.com>
2025-11-07 20:22:42 -0500

    Minor change in Type

--- src/main/java/org/example/Type.java
+++ src/main/java/org/example/Type.java
@@ -7,18 +7,28 @@ public enum Type {
     COLOSSUS("calash",90,90),
     DWARF("nick",90,90);
 
-    private final Weapon weapon;
     private final int startingHP;
+    private final int maxHP;
+    private final int weaponPower;
+    private final String weaponName;
     Type(String weaponName, int weaponPower,int startingHP){
-        this.weapon=new Weapon(weaponName,weaponPower);
+        this.weaponPower = weaponPower;
         this.startingHP=startingHP;
+        this.maxHP=startingHP;
+        this.weaponName=weaponName;
     }
 
     public int getStartingHP() {
         return this.startingHP;
     }
-    public Weapon getWeapon(){
-        return this.weapon;
+    public int getWeaponPower(){
+        return this.weaponPower;
+    }
+    public int getMaxHP(){
+        return this.maxHP;
+    }
+    public String getWeaponName(){
+        return this.weaponName;
     }
 
 }
