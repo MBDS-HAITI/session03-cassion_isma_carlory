978c76e7d035eb0cd4ba59f76f44b1284c425b51
ismamay <ismamayy@yahoo.fr>
2025-11-08 18:03:17 -0500

    finalisation classe Battle

--- src/main/java/org/example/Battle.java
+++ src/main/java/org/example/Battle.java
@@ -1,9 +1,14 @@
 package org.example;
 
+import java.util.List;
+import java.util.Scanner;
+
 public class Battle {
     private Player player1;
     private Player player2;
     private int countRound = 0;
+
+    private final Scanner scanner = new Scanner(System.in);
 
     public Battle(Player player1, Player player2) {
         this.player1 = player1;
@@ -13,7 +18,7 @@ public class Battle {
     public void start() {
         System.out.println("====== Le Combat a commencé======");
         Player actif = player1;
-        Player Opponent = player2;
+        Player opponent = player2;
 
         while (!player1.isDefeated() && !player2.isDefeated())
         {
@@ -22,17 +27,52 @@ public class Battle {
 
             displayTeams();
 
-           /* boolean active = player1.isDefeated();
-            if(!active) {
-                System.out.println(player1.getName() + "n'a plus de caracteres en vie!");
+            Character active =  chooseAlveCharacter(actif,scanner);
+            if(active==null) {
+                System.out.println(actif.getName() + "n'a plus de caracteres en vie!");
                 break;
-            }*/
+            }
 
+            if(active instanceof Healer healer){
+                System.out.println("Choose action: 1- Attack  2- Heal");
+                String choice = scanner.nextLine();
 
+                if (choice.equals("2")) {
+                    Character ally= chooseAlveCharacter(actif,scanner);
+                    healer.heal(ally);
+                }
+                else{
+                    Character enemy = chooseAlveCharacter(opponent,scanner);
 
+                    if (active instanceof  Attacker attacker){
+                        attacker.attack(enemy);
+                    }
+                    else {
+                        System.out.println(active.getName() + "ne peut pas attaquer!");
+                    }
 
+                }
+
+            }
+            else  if (active instanceof Attacker attacker){
+                Character enemy = chooseAlveCharacter(opponent,scanner);
+                attacker.attack(enemy);
+            }
+
+            //verify if the opponet is defeated
+
+            if (opponent.isDefeated()) {
+                System.out.println(actif.getName() + "a gagne apres " + countRound + " rounds" );
+                break;
+            }
+
+            Player temp = actif;
+            actif = opponent;
+            opponent = temp;
 
         }
+        displayFinalStatus();
+
 
     }
 
@@ -47,6 +87,67 @@ public class Battle {
         player2.getCharacters().forEach(character ->
                 System.out.println(" - "+character.getName() + character.getHp()+ "HP"));
 
+    }
+
+    private Character chooseAlveCharacter(Player player, Scanner scanner)
+    {
+        List<Character> aliveCharacters = player.getCharacters();
+        if(aliveCharacters.isEmpty())
+        {
+            System.out.println(" Aucun personnage vivant pour "+player.getName());
+            return null;
+        }
+        //show alive characters
+
+        System.out.println("\n"+ player.getName()+ ", chosis un personnage");
+        for(int i=0;i<aliveCharacters.size();i++){
+            Character c= aliveCharacters.get(i);
+            System.out.printf("[%d] %s (%s, %d HP) %n",i+1,c.getName(), c.getType().name(),c.getHp());
+        }
+
+        //read and validate the choice
+
+        int choice = -1;
+        while(true)
+        {
+            System.out.print("Entrez le numero de ton choix: ");
+            try{
+                choice = Integer.parseInt(scanner.nextLine().trim());
+                if(choice>=1 && choice<=aliveCharacters.size())
+                {
+                    break;
+                }
+                else {
+                    System.out.println(" Choix invalide. reessayez !");
+
+                }
+            }
+            catch (NumberFormatException e){
+                System.out.println(" valeur saisie invalide. entrez un nombre ");
+            }
+        }
+        Character chosen = aliveCharacters.get(choice-1);
+        System.out.println(chosen.getName() + "selectionne !");
+        return chosen;
+
+    }
+     private void displayFinalStatus(){
+         System.out.println("\n=== Resume de la bataille ===");
+         System.out.println("ROund total : "+countRound+ "\n");
+
+         showTeamStatus(player1);
+         showTeamStatus(player2);
+     }
+
+
+
+    private  void showTeamStatus(Player player){
+        System.out.println("Equipe "+ player.getName() + " : ");
+        for (Character character : player.getCharacters()) {
+            String status = character.isAlive() ? "Vivant": "Mort";
+            System.out.println(" - "+ character.getName() +  " (" + character.getClass().getSimpleName() + "): " +character.getHp()+ "HP - "+ status);
+        }
+        System.out.println();
     }
 
 
